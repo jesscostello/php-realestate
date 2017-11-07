1a5a5a6f8cbbeac628c88c81c19c826db11d695e
jesscostello <33467471+jesscostello@users.noreply.github.com>
2017-11-08 10:47:47 +1300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,26 @@
+﻿function fetchSelectedCity(val) {
+ $.ajax({
+ 	type: 'POST',
+ 	url: 'fetchSelectedCity.php',
+ 	data: {
+  		propertyCity:val
+ 	},
+ success: 	function (response) {
+ 		 		document.getElementById("propertySuburb").innerHTML=response; 
+ 			} // end of function(response)
+ }); // end of ajax
+} // end of function fetchSelectedCity
+
+
+function fetchSelectedCityEdit(val) {
+ $.ajax({
+ 	type: 'POST',
+ 	url: 'fetchSelectedCityEdit.php',
+ 	data: {
+  		propertyCity:val
+ 	},
+ success: 	function (response) {
+ 		 		document.getElementById("propertySuburb").innerHTML=response; 
+ 			} // end of function(response)
+ }); // end of ajax
+} // end of function fetchSelectedCityEdit
